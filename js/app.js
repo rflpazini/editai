5a62e9484d59d3bc73c3148235b828df3eb2fdc9
akrafzik <alec.k.haroutiounian@hotmail.com>
2017-02-01 15:47:15 -0200

    app.js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var lock = new Auth0Lock('Sbu2Ngdr1g2D3aFBKdojLQGUo2patuqF', 'editai.auth0.com');
+var btn_login = document.getElementById('btn-login');
+
+btn_login.addEventListener('click', function() {
+    lock.show();
+});
+
+lock.on("authenticated", function(authResult) {
+    lock.getProfile(authResult.idToken, function(error, profile) {
+        if (error) {
+            // Handle error
+            return;
+        }
+        localStorage.setItem('id_token', authResult.idToken);
+        // Display user information
+        show_profile_info(profile);
+    });
+});
