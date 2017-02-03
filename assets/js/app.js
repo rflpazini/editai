b5457c4a51773c95a5ae5fbfe1326a311d7cc7f2
Rafael Pazini <rflpazini@gmail.com>
2017-02-03 00:59:16 -0200

    add login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,65 @@
 var options = {
-    language: 'pt-br',
+
     theme: {
-        logo: "http://imageshack.com/a/img922/3457/MOs0ty.png",
+        logo: "https://dl.dropboxusercontent.com/u/104095732/16388036_1703660259963949_4229443957888103359_n.jpg",
         primaryColor: "#bf0202"
     },
     languageDictionary: {
         emailInputPlaceholder: "editaistudio@gmail.com",
-        title: " "
-    },
+    }
 };
 
 // Initiating our Auth0Lock
 var lock = new Auth0Lock(
     'Sbu2Ngdr1g2D3aFBKdojLQGUo2patuqF',
-    'editai.auth0.com',
-    options,
+    'editai.auth0.com'
 );
-var btn_login = document.getElementById('btn-login');
+
+var btn_login = document.getElementById('bt-login');
+
 btn_login.addEventListener('click', function() {
-    lock.show();
+    lock.show({
+        closable: false,
+        allowedConnections: ["facebook"],
+        theme: { "logo": "https://dl.dropboxusercontent.com/u/104095732/16388036_1703660259963949_4229443957888103359_n.jpg", "primaryColor": "#bf0202" },
+        language: "pt-br",
+        auth: {
+            responseType: 'token',
+            params: {
+                scope: 'openid email'
+            }
+        }
+    });
 });
 
 lock.on("authenticated", function(authResult) {
     lock.getProfile(authResult.idToken, function(error, profile) {
         if (error) {
-            // Handle error
             return;
         }
         localStorage.setItem('id_token', authResult.idToken);
-        // Display user information
         show_profile_info(profile);
     });
 });
+
+var retrieve_profile = function() {
+    var id_token = localStorage.getItem('id_token');
+    if (id_token) {
+        lock.getProfile(id_token, function(err, profile) {
+            if (err) {
+                return alert('There was an error getting the profile: ' + err.message);
+            }
+            // Display user information
+            show_profile_info(profile);
+        });
+    }
+};
+
+var show_profile_info = function(profile) {
+    var avatar = document.getElementById('avatar');
+    document.getElementById('nickname').textContent = profile.nickname;
+    btn_login.style.display = "none";
+    avatar.src = profile.picture;
+    avatar.style.display = "block";
+    btn_logout.style.display = "block";
+};
